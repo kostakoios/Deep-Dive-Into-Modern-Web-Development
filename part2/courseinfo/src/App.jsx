03a3ff7c763c524d4d862ac3ff3cd9557cadbf0c
konstantin tedoradze <kostat@koiostech.ai>
2024-04-06 10:51:28 +0400

    2.5: Separate module step10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,37 +1,5 @@
 import Course from './components/Course'
 
-const Header = (props) => {
-  console.log(props)
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  ) 
-}
-
-const Part = (props) => {
-  console.log(props)
-  return (
-    <p>
-        {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
 const App = () => {
   const courses = [
     {
