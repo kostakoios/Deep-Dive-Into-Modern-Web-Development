6c43b64e7bd91cad18afa42b2f7288224f6199e9
konstantin tedoradze <kostat@koiostech.ai>
2024-04-06 10:30:17 +0400

    2.3*: Course information step8

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -3,8 +3,7 @@ import Part from './Part'
 const Content = ({ parts }) => {
     console.log('parts: ', parts)
 
-    let total = 0
-    parts.map(part => total += part.exercises)
+    const total = parts.reduce((s, p) => s + p.exercises, 0)
     return (
         <div>
             {parts.map(part => <Part key={part.id} part={part}/>)}
