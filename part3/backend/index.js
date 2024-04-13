169a314f296d1982d69af2a540baf8bf8be79f35
konstantin tedoradze <kostat@koiostech.ai>
2024-04-13 19:38:02 +0400

    Exercises 3.7.-3.8.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.json());
+app.use(morgan('tiny'))
 
 let persons = [
   { 
@@ -25,6 +27,20 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
   
 app.get("/api/persons", (request, response) => {
   response.json(persons);
@@ -77,6 +93,7 @@ app.delete("/api/persons/:id", (request, response) => {
 
   response.status(204).end();
 })
+app.use(unknownEndpoint)
 
 const PORT = 3001;
 app.listen(PORT, () => {
