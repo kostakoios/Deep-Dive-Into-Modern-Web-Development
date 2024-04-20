c44e55d5a8b9d8eece87986b3de32f95b94528c5
konstantin tedoradze <kostat@koiostech.ai>
2024-04-20 19:02:16 +0400

    3.16: Phonebook database, step 4

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -32,16 +32,16 @@ app.get("/", (request, response) => {
   response.send("<h1>Ruska is my queen</h1>");
 });  
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Persons.find({}).then(persons => response.json(persons))
-  .catch(err => response.status(400).send({ error: err.message}));
+  .catch(error => next(error));
 });
 
 const generateId = () => {
   return  Math.floor(Math.random() * 1000);
 }
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   let newId = generateId();
   let body = request.body;
   if(!body.name || !body.number){
@@ -63,7 +63,7 @@ app.post("/api/persons", (request, response) => {
     .then(savedPerson => response.json(savedPerson))
     .catch(err => response.status(400).end({error: err.message}));
   })
-  .catch(err => response.status(400).end({error: err.message}));
+  .catch(error => next(error));
 });
 
 app.get("/info", (request, response) => {
@@ -72,14 +72,14 @@ app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${sumOfPersons} people</p> <p>${date}</p>`);
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Persons.findById(request.params.id).then(person => {
     response.json(person);
   })
-  .catch(err => response.status(400).json({error: err.message}));
+  .catch(error => next(error));
 });
 
-app.put("/api/persons/:id", async (request, response) => {
+app.put("/api/persons/:id", async (request, response, next) => {
   let body = request.body;
   if(!body.name || !body.number){
     return response.status(400).json({ 
@@ -94,18 +94,27 @@ app.put("/api/persons/:id", async (request, response) => {
   .then(person => {
     return response.json(person);
   })
-  .catch(err => response.status(400).json({error: err.message}));
-
+  .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", async (request, response) => {
+app.delete("/api/persons/:id", async (request, response, next) => {
   await Persons.findByIdAndDelete(request.params.id).then(person => {
      response.json(person);
-  }).catch(err => response.status(400).json({error: err.message}));
-})
+  }).catch(error => next(error));
+});
 
 
 app.use(unknownEndpoint)
+
+const errorHendler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+
+app.use(errorHendler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
