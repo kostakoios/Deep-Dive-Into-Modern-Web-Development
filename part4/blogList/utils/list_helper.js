ecd3306b68e9b905af4faa4745378072a8de88f1
konstantin tedoradze <kostat@koiostech.ai>
2024-05-22 11:37:34 +0400

    4.6*: Helper Functions and Unit Tests, step 4

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -19,8 +19,26 @@ const favoriteBlog = (array) => {
   return {title: obj.title, author: obj.author, likes: obj.likes}
 }
 
+const mostBlogs = (array) => {
+  if (array.length === 0) {
+    return 0;
+  }
+  const authors = {};
+  array.forEach(el => {
+    if (authors.hasOwnProperty(el.author)) {
+      authors[el.author] = authors[el.author] + 1; 
+    } else {
+      console.log("author name:", el.author);
+      authors[el.author] = 1;
+    }
+  })
+  const finalResult = Object.entries(authors).reduce((a, b) => authors[a] > authors[b] ? a : b);
+  return {author: finalResult[0], blogs: finalResult[1]};
+} 
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 };
