bb4922177f533ecf3d52d622abaca96f8d627cf7
konstantin tedoradze <kostat@koiostech.ai>
2024-05-22 10:32:55 +0400

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -10,8 +10,17 @@ const totalLikes = (array) => {
   return array.length === 0 ? 0 : array.reduce(reducer, 0);
 };
 
+const favoriteBlog = (array) => {
+  if (array.length === 0) {
+    return 0;
+  }
+  const reducer = (prev, current) => (prev && prev.likes > current.likes) ? prev : current;
+  let obj = array.reduce(reducer);   
+  return {title: obj.title, author: obj.author, likes: obj.likes}
+}
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 };
