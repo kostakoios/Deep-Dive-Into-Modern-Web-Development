370dbe0f15eb0d15a134d494a23340dafd73c2b8
rusudan jojishvili <rosjojishvili@gmail.com>
2024-06-16 15:02:25 +0400

    4.9: Blog List Tests, step 2

--- part4/blogList/tests/blog-api.test.js
+++ part4/blogList/tests/blog-api.test.js
@@ -2,14 +2,26 @@ const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const assert = require('assert');
 
 const api = supertest(app)
 
-test.only('notes are returned as json', async () => {
+test.only('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test.only('unique identifier property of the blog posts is named id', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    // Verify each blog post in the response has an `id` property
+    response.body.forEach(blog => {
+        assert.ok(blog.id)
+    })
 })
 
 after(async () => {
