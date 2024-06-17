a55dd560a395a09da02bd6cb833e391d0a36e377
kosta tedoradze <kostat@koiostech.ai>
2024-06-17 16:26:44 +0400

    4.15: Blog List Expansion, step 3

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -1,18 +1,17 @@
 "use strict"
-
-const config = require('./utils/config');
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const middleware = require('./utils/middleware');
-const logger = require('./utils/logger');
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const blogListRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-
-logger.info('connecting to', config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -20,37 +19,16 @@ mongoose.connect(config.MONGODB_URI)
   })
   .catch((error) => {
     logger.error('error connecting to MongoDB:', error.message)
-  });
+  })
 
 app.use(cors())
 app.use(express.json())
-app.use(middleware.requestLogger);
+app.use(middleware.requestLogger)
 
-app.use('/api/blogs', blogListRouter);
+app.use('/api/blogs', blogListRouter)
+app.use('/api/users', usersRouter)
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app;
-// app.get('/api/blogs', (request, response) => {
-//   Blog
-//     .find({})
-//     .then(blogs => {
-//       response.json(blogs)
-//     })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog
-//     .save()
-//     .then(result => {
-//       response.status(201).json(result)
-//     })
-// })
-
-// const PORT = 3003
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
