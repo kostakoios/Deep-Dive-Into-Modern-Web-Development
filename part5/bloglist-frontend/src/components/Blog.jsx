f0ed4ec87c0a6fd6431a7c5918622901329f25ca
kosta tedoradze <kostat@koiostech.ai>
2024-07-19 16:41:51 +0400

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} 
+      <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'} </button>
+      {visible && (
+        <div>
+        <p>{blog.url}</p>
+        <p>{blog.likes}</p>
+        <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
