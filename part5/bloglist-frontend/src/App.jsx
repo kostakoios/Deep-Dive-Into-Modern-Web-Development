ab6a7157e81a979c77533ea40107d55dba540955
kosta tedoradze <kostat@koiostech.ai>
2024-07-19 18:32:20 +0400

    5.10: Blog List Frontend, step 10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -150,7 +150,7 @@ const App = () => {
       <p>{user.name} logged in <button onClick={handleLogout}>Logout</button></p>
 
       {blogForm()}
-      {blogs.map(blog =>
+      {blogs.sort((a,b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} updateBlogLikes={updateBlogLikes} />
       )}
     </div>
