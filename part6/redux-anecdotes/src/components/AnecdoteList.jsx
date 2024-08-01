d8af3c541619a92aaad7f28b208c86da53c6bf41
kosta tedoradze <kostat@koiostech.ai>
2024-08-01 17:14:53 +0400

    6.9 Better Anecdotes, step 7

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
 
-
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(({anecdotes, filter}) => filter === '' ? anecdotes 
+    : anecdotes.filter(anecdot => {
+        if (anecdot.content.includes(filter)) {
+            return anecdot
+        }
+    }))
     const dispatch = useDispatch()
 
     const vote = (id) => {
