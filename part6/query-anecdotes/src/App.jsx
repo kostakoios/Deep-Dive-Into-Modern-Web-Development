40f8432f2efb9e05661f736ef26eacc1ffae5415
kosta tedoradze <kostat@koiostech.ai>
2024-08-06 15:07:26 +0400

    Exercise 6.22

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,13 +1,22 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { getAnecdotes, createAnecdote } from './requests'
+import { getAnecdotes, createAnecdote, updateVote } from './requests'
 
 const App = () => {
   const queryClient = useQueryClient()
 
+  const updateVoteMutation = useMutation({
+    mutationFn: updateVote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
+  
   const handleVote = (anecdote) => {
     console.log('vote')
+    updateVoteMutation.mutate({...anecdote, votes: anecdote.votes + 1 })
   }
 
   const newAnecdoteMutation = useMutation({ 
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -7,3 +7,6 @@ export const getAnecdotes = () =>
 
 export const createAnecdote = (anecdote) => 
     axios.post(baseUrl, anecdote).then(res => res.data)
+
+export const updateVote = updatedVote =>
+    axios.put(`${baseUrl}/${updatedVote.id}`, updatedVote).then(res => res.data)
