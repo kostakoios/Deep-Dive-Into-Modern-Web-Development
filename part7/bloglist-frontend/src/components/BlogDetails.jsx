74b58616f6d99947c54079c3d8be8642c5805556
kosta tedoradze <kostat@koiostech.ai>
2024-08-23 17:53:31 +0400

    7.19: Comments, step 2

--- part7/bloglist-frontend/src/components/BlogDetails.jsx
+++ part7/bloglist-frontend/src/components/BlogDetails.jsx
@@ -1,15 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import blogService from '../services/blogs'
-import LogedInUser from './LogedInUser'
 import { useDispatch } from 'react-redux'
 import { appendUser } from "../reducers/userReducer";
-import { updateBlogLikes as updateBlogLikesAction } from '../reducers/bloglistReducer';
+import { updateBlogComment, updateBlogLikes as updateBlogLikesAction } from '../reducers/bloglistReducer';
 
 const BlogDetails = () => {
   const dispatch = useDispatch()
   const blogId = useParams().id
   const [blogInfo, setBlogInfo] = useState(null)
+  const [newComment, setNewComment] = useState('')
   const loggedUserJson = window.localStorage.getItem("loggedBlogUser");
 
   useEffect(() => {
@@ -34,6 +34,23 @@ const BlogDetails = () => {
     setBlogInfo(updatedObject); // Update the local state to reflect the new likes count
   };
 
+  const addComment = async (event) => {
+    event.preventDefault()
+    if(newComment !== '') {
+      const returnedBlog = await blogService.createComment(blogId, {comment: newComment});
+        // Update the local state with the new comment
+    setBlogInfo((prevBlogInfo) => ({
+      ...prevBlogInfo,
+      comments: prevBlogInfo.comments.concat(newComment)
+    }));
+      dispatch(updateBlogComment(returnedBlog));
+      setNewComment('')
+    } else {
+      alert('Enter new comment than click on add comment!')
+    }
+    
+  }
+
   return (
     <div>
       <h1>{blogInfo.title}</h1>
@@ -42,6 +59,15 @@ const BlogDetails = () => {
       <button onClick={handleBlogLikes}>like</button>
       <p>Added by {blogInfo.author}</p>
       <h3>Comments</h3>
+      <form onSubmit={addComment}>
+        <input type="text"
+          value={newComment}
+          name="comment"
+          placeholder="add comment"
+          onChange={({ target }) => setNewComment(target.value)}
+         />
+        <button>add Comment</button>
+      </form>
       <ul>
       {blogInfo.comments && blogInfo.comments.map((comment, index) => <li key={index}>{comment}</li> )}
       </ul>
